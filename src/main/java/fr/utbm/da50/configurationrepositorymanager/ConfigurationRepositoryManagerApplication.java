fff56339e6d47788a94c5cc5f987c941093d97d3
BuildTools <unconfigured@null.spigotmc.org>
2021-10-26 18:37:45 +0200

    first commit

---
+++ src/main/java/fr/utbm/da50/configurationrepositorymanager/ConfigurationRepositoryManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.utbm.da50.configurationrepositorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationRepositoryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationRepositoryManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/utbm/da50/configurationrepositorymanager/ConfigurationRepositoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utbm.da50.configurationrepositorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationRepositoryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
